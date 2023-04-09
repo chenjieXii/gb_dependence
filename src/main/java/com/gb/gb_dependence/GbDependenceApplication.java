841c336b7f4cbc3580627468742ac0d574a4fb3e
houyouwen <houyouwen@163.com>
2023-04-09 22:41:01 +0800

    feat:初始化提交

---
+++ src/main/java/com/gb/gb_dependence/GbDependenceApplication.java
@@ -0,0 +1,13 @@
+package com.gb.gb_dependence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbDependenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbDependenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gb/gb_dependence/GbDependenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gb.gb_dependence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbDependenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
